174eddc542774cf4bedfbaba5f9c6747be6c4d49
TsoIsTheWayToGo <ericgordontso@gmail.com>
2020-03-28 13:32:39 -0700

    learn useEffect

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import { Text, StyleSheet, View, Flatlist, Button, TextInput, ToucableOpacity } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -7,35 +7,42 @@ const SearchScreen = () => {
 	const [term, setTerm] = useState('');
 	const [results, setResults] = useState([]);
 	//error handling
-	const [errorMessage, setErrorMessage] = useState('')
-
+	const [errorMessage, setErrorMessage] = useState('');
+	// console.log('^');
+	
 	// api call
-	const searchApi = async () => {
+	const searchApi = async searchTerm => {
+		// console.log('hi there');
 		try {
-		const response = await yelp.get('/search', {
-			params: {
-				limit: 50,
-				term: term, 
-				location: 'san jose'
-			}
-		});
-		setResults(response.data.businesses);
-	} catch(err) {
-		setErrorMessage('Something Went Wrong')
-	}
+			const response = await yelp.get('/search', {
+				params: {
+					limit: 10,
+					term: term,
+					location: 'san jose',
+				},
+			});
+			setResults(response.data.businesses);
+		} catch (err) {
+			setErrorMessage('Something Went Wrong');
+		}
 	};
+
+	// const initialData = searchApi('resturant')
+
+	//dont do this it causes infinite loop
+	// searchApi('pasta')
+
+	useEffect(() => {
+		searchApi('pasta');
+	}, [ ]);
 
 	return (
 		<View>
-			<SearchBar
-				term={term}
-				onTermChange={newTerm => setTerm(newTerm)}
-				onTermSubmit={()=>searchApi()}
-			/>
-			<Text>We Have found {results.length} resturants.</Text>
-		{errorMessage ? <Text>{errorMessage}</Text> : null }
-			 
-			
+			<SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} onTermSubmit={() => searchApi(term)} />
+			{/* <Text>We Have found {results.length} resturants.</Text> */}
+			{/* <Flatlist data={results} renderItem={(item)=> item.name}/>  */}
+			{results.map((item) => <Text>{item.name}</Text>)}
+			{errorMessage ? <Text>{errorMessage}</Text> : null}
 		</View>
 	);
 };
