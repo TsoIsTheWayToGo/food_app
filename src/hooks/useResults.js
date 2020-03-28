39c19c33128d2a42cdd193cfc571236e4ff2bdf5
TsoIsTheWayToGo <ericgordontso@gmail.com>
2020-03-28 14:39:35 -0700

    Make a hook

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,1 +1,36 @@
-import {useEffect, useState}
+import { useEffect, useState } from 'react';
+import yelp from '../api/yelp'
+
+export default () => {
+  const [results, setResults] = useState([]);
+  //error handling
+  const [errorMessage, setErrorMessage] = useState('');
+  // console.log('^');
+
+  // api call
+  const searchApi = async searchTerm => {
+		// console.log('hi there');
+		try {
+			const response = await yelp.get('/search', {
+				params: {
+					limit: 10,
+					term: searchTerm,
+					location: 'san jose',
+				},
+			});
+			setResults(response.data.businesses);
+		} catch (err) {
+			setErrorMessage('Something Went Wrong');
+		}
+  };
+
+  // const initialData = searchApi('resturant')
+
+  //dont do this it causes infinite loop
+  // searchApi('pasta')
+
+  useEffect(() => {
+		searchApi('pasta');
+  }, []);
+  return [searchApi, results, errorMessage]
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,46 +2,18 @@ import React, { useState, useReducer, useEffect } from 'react';
 import { Text, StyleSheet, View, Flatlist, Button, TextInput, ToucableOpacity } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState('');
-	const [results, setResults] = useState([]);
-	//error handling
-	const [errorMessage, setErrorMessage] = useState('');
-	// console.log('^');
-	
-	// api call
-	const searchApi = async searchTerm => {
-		// console.log('hi there');
-		try {
-			const response = await yelp.get('/search', {
-				params: {
-					limit: 10,
-					term: term,
-					location: 'san jose',
-				},
-			});
-			setResults(response.data.businesses);
-		} catch (err) {
-			setErrorMessage('Something Went Wrong');
-		}
-	};
-
-	// const initialData = searchApi('resturant')
-
-	//dont do this it causes infinite loop
-	// searchApi('pasta')
-
-	useEffect(() => {
-		searchApi('pasta');
-	}, [ ]);
+	const [searchApi, results, errorMessage] = useResults();
 
 	return (
 		<View>
 			<SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} onTermSubmit={() => searchApi(term)} />
-			{/* <Text>We Have found {results.length} resturants.</Text> */}
+			<Text>We Have found {results.length} resturants.</Text>
 			{/* <Flatlist data={results} renderItem={(item)=> item.name}/>  */}
-			{results.map((item) => <Text>{item.name}</Text>)}
+			{/* {results.map((item) => <Text>{item.name}</Text>)} */}
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 		</View>
 	);
@@ -50,3 +22,55 @@ const SearchScreen = () => {
 const styles = StyleSheet.create({});
 
 export default SearchScreen;
+// import React, { useState, useReducer, useEffect } from 'react';
+// import { Text, StyleSheet, View, Flatlist, Button, TextInput, ToucableOpacity } from 'react-native';
+// import SearchBar from '../components/SearchBar';
+// import yelp from '../api/yelp';
+
+// const SearchScreen = () => {
+// 	const [term, setTerm] = useState('');
+// const [results, setResults] = useState([]);
+// //error handling
+// const [errorMessage, setErrorMessage] = useState('');
+// // console.log('^');
+
+// // api call
+// const searchApi = async searchTerm => {
+// 	// console.log('hi there');
+// 	try {
+// 		const response = await yelp.get('/search', {
+// 			params: {
+// 				limit: 10,
+// 				term: term,
+// 				location: 'san jose',
+// 			},
+// 		});
+// 		setResults(response.data.businesses);
+// 	} catch (err) {
+// 		setErrorMessage('Something Went Wrong');
+// 	}
+// };
+
+// // const initialData = searchApi('resturant')
+
+// //dont do this it causes infinite loop
+// // searchApi('pasta')
+
+// useEffect(() => {
+// 	searchApi('pasta');
+// }, [ ]);
+
+// 	return (
+// 		<View>
+// 			<SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} onTermSubmit={() => searchApi(term)} />
+// 			{/* <Text>We Have found {results.length} resturants.</Text> */}
+// 			{/* <Flatlist data={results} renderItem={(item)=> item.name}/>  */}
+// 			{results.map((item) => <Text>{item.name}</Text>)}
+// 			{errorMessage ? <Text>{errorMessage}</Text> : null}
+// 		</View>
+// 	);
+// };
+
+// const styles = StyleSheet.create({});
+
+// export default SearchScreen;
