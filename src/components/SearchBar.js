c089b2c4f376a847bccce36db268349bca676c95
TsoIsTheWayToGo <ericgordontso@gmail.com>
2020-03-28 10:28:56 -0700

    Create search screen

---
+++ src/components/SearchBar.js
@@ -0,0 +1,2 @@
+// import React, { useState, useReducer } from 'react';
+// import { Text, StyleSheet, View, Flatlist, Button, TextInput, ToucableOpacity } from 'react-native';
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,19 @@
+import React, { useState, useReducer } from 'react';
+import { Text, StyleSheet, View, Flatlist, Button, TextInput, ToucableOpacity } from 'react-native';
+
+const SearchScreen = () => {
+
+  return (
+    <View>
+      <Text>
+        Search Screen
+      </Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+
+})
+
+export default SearchScreen; 
