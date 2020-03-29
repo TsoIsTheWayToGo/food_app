4f93029b66e9004173919534166c612e9e97eabb
TsoIsTheWayToGo <ericgordontso@gmail.com>
2020-03-28 18:10:31 -0700

    Got images to show

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,13 +1,42 @@
-import React, { useState, useReducer } from 'react';
-import { Text, StyleSheet, Image, View, Flatlist, Button, TextInput } from 'react-native';
-
+import React, { useState, useReducer, useEffect } from 'react';
+import { Text, StyleSheet, Image, View, FlatList, Button, TextInput } from 'react-native';
+import yelp from '../api/yelp';
 const ResultsShowScreen = ({ navigation }) => {
-  const id = navigation.getParam('id');
-  console.log(id);
-  
-	return <Text>Pictures</Text>;
+	const id = navigation.getParam('id');
+	const [result, setResult] = useState(null);
+
+	const getResult = async id => {
+		const response = await yelp.get(`/${id}`);
+		setResult(response.data);
+	};
+	useEffect(() => {
+		getResult(id);
+	}, []);
+
+	if (!result) {
+		return null;
+	}
+	// console.log(result.phone);
+
+	return (
+		<>
+			<Text>{result.name}</Text>
+			<FlatList
+				data={result.photos}
+				keyExtractor={(photo) => photo}
+				renderItem={({item}) => {
+					return <Image style={styles.image} source={{uri: item}} />;
+				}}
+			/>
+		</>
+	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 200
+  }
+});
 
 export default ResultsShowScreen;
